38760cbf3a8c13c1d767a984e1d1f8e0cf44da5f
GitHub Action <action@github.com>
2023-06-20 22:22:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sloppylopez.moneypennyideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
