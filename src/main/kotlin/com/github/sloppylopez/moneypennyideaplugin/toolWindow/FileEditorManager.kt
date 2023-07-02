5c7032fd1b0fa7211c93a3adcda3cf91f5bbb029
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-07-03 00:36:57 +0200

    :children_crossing: Add intention to all editors

--- src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/toolWindow/FileEditorManager.kt
+++ src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/toolWindow/FileEditorManager.kt
@@ -1,24 +1,21 @@
 package com.github.sloppylopez.moneypennyideaplugin.toolWindow
 
-import com.intellij.codeInsight.intention.IntentionAction
-import com.intellij.codeInsight.intention.IntentionManager
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.diagnostic.thisLogger
 import java.io.File
-import javax.swing.SwingUtilities
 import javax.swing.SwingUtilities.invokeLater
 
 @Service(Service.Level.PROJECT)
 class FileEditorManager(private val project: Project) {
+    private val refactorIntentionFactory = project.service<RefactorIntentionFactory>()
 
     fun openFileInEditor(filePath: String?) {
         if (filePath == null) return
@@ -33,42 +30,7 @@ class FileEditorManager(private val project: Project) {
                         "        thisLogger().warn(\"Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.\")\n" +
                         "    }"
             )
-            invokeLater {
-                val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
-                if (psiFile != null) {
-                    ApplicationManager.getApplication().invokeLater({
-                        addCustomIntention()
-                    }, ModalityState.NON_MODAL)
-                }
-            }
         }
-    }
-
-    private fun addCustomIntention() {
-        val intentionManager = IntentionManager.getInstance()
-
-        // Create a new IntentionAction for "MoneyPenny Refactor"
-        val customIntention = object : IntentionAction {
-            override fun getText(): String = "MoneyPenny Refactor1"
-
-            override fun getFamilyName(): String = "Custom Intentions"
-
-            override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean = true
-
-            override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-                // Show a message when the intention is selected
-                val message = "Hello World"
-                ApplicationManager.getApplication().runWriteAction {
-                    editor?.let { selectedEditor ->
-                        val offset = selectedEditor.caretModel.offset
-                        selectedEditor.document.insertString(offset, message)
-                    }
-                }
-            }
-
-            override fun startInWriteAction(): Boolean = false
-        }
-        intentionManager.addAction(customIntention)
     }
 
     private fun highlightTextInEditor(text: String) {
--- src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/toolWindow/ToolWindowFactory.kt
+++ src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/toolWindow/ToolWindowFactory.kt
@@ -1,12 +1,17 @@
 package com.github.sloppylopez.moneypennyideaplugin.toolWindow
 
 import MoneyPennyToolWindow
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 import javax.swing.ImageIcon
+import javax.swing.SwingUtilities.invokeLater
+
 
 class ToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(
@@ -14,6 +19,15 @@ class ToolWindowFactory : ToolWindowFactory {
         toolWindow: ToolWindow
     ) {
         try {
+            val refactorIntentionFactory = project.service<RefactorIntentionFactory>()
+            invokeLater {
+                ApplicationManager.getApplication().invokeLater(
+                    {
+                        refactorIntentionFactory.addCustomIntentionToAllEditors()
+                    },
+                    ModalityState.NON_MODAL
+                )//TODO: if we drag a file before indexing we get an error here reported on intellij idea console
+            }
             toolWindow.setIcon(getToolWindowIcon())
             val moneyPennyToolWindow = MoneyPennyToolWindow(project, toolWindow)
 
