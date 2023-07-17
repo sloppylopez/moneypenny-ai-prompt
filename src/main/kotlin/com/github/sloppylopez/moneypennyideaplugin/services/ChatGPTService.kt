fbe516d2c3bc06da0313985dc37ae1a9382c448b
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-07-17 19:40:02 +0200

    :wheelchair: Add progress bar indicator

--- src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/services/ChatGPTService.kt
+++ src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/services/ChatGPTService.kt
@@ -13,6 +13,7 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.nio.charset.StandardCharsets
 import java.time.Duration
+import java.util.concurrent.CompletableFuture
 
 @Service(Service.Level.PROJECT)
 class ChatGPTService(project: Project) {
@@ -26,56 +27,50 @@ class ChatGPTService(project: Project) {
         requireNotNull(apiKey) { "API key not found in environment variables." }
     }
 
-    fun sendChatPrompt(prompt: String, callback: ChatGptChoiceCallback) {
+    fun sendChatPrompt(prompt: String, callback: ChatGptChoiceCallback): CompletableFuture<ChatGptCompletion> {
         val requestBody = getRequestBodyJson(prompt)
-        val request = HttpRequest.newBuilder()
+        val request = createHttpRequest(requestBody)
+        return sendAsyncRequest(request)
+            .thenApply { obj: HttpResponse<String>? -> obj?.body() }
+            .thenApply { responseBody: String? ->
+                gson.fromJson(responseBody, ChatGptCompletion::class.java)
+            }
+            .whenComplete { choice: ChatGptCompletion?, throwable: Throwable? ->
+                handleCompletion(choice, throwable, callback)
+            }
+    }
+
+    private fun createHttpRequest(requestBody: String): HttpRequest =
+        HttpRequest.newBuilder()
             .uri(URI.create("https://api.openai.com/v1/chat/completions"))
             .timeout(Duration.ofSeconds(60))
             .header("Authorization", "Bearer $apiKey")
             .header("Content-Type", "application/json; charset=utf-8")
             .POST(HttpRequest.BodyPublishers.ofString(requestBody, StandardCharsets.UTF_8))
             .build()
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-            .thenApply { obj: HttpResponse<String?> -> obj.body() }
-            .thenApply { responseBody: String? ->
-                gson.fromJson(
-                    responseBody,
-                    ChatGptCompletion::class.java
-                )
-            }
-            .whenComplete { choice: ChatGptCompletion?, throwable: Throwable? ->
-                if (throwable != null) {
-                    service.showNotification("Error", throwable.message!!)
-                    callback.onCompletion(ChatGptMessage("system", "Error: ${throwable.message}"))
-                } else {
-                    val message = getMessage(choice)
-                    callback.onCompletion(message)
-                }
-            }
+
+    private fun sendAsyncRequest(request: HttpRequest): CompletableFuture<HttpResponse<String>> {
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+    }
+
+    private fun handleCompletion(
+        choice: ChatGptCompletion?,
+        throwable: Throwable?,
+        callback: ChatGptChoiceCallback
+    ) {
+        if (throwable != null) {
+            service.showNotification("Error", throwable.message!!)
+            callback.onCompletion(ChatGptMessage("system", "Error: ${throwable.message}"))
+        } else {
+            val message = getMessage(choice)
+            callback.onCompletion(message)
+        }
     }
 
     private fun getMessage(choice: ChatGptCompletion?) =
         choice?.choices?.get(0)?.message ?: ChatGptMessage("system", "Error: No response from GPT")
 
-    private fun getRequestBody(prompt: String): String {
-        val escapedPrompt = service.escapePrompt(prompt)
-        val lineEscapedPrompt = service.escapePromptLines(escapedPrompt)
-        val promptLength = escapedPrompt.length
-        val maxTokenCount = 4096 - 1 - promptLength
-
-        val requestBody = """
-                {
-                    "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "system", "content": "You are a helpful assistant. always answer without explanations, return only code if possible"}, {"role": "user", "content": "$lineEscapedPrompt"}],
-                    "max_tokens": $maxTokenCount
-                }
-            """
-        return requestBody
-    }
-
     private fun getRequestBodyJson(prompt: String): String {
-//        val escapedPrompt = service.escapePrompt(prompt)
-//        val lineEscapedPrompt = service.escapePromptLines(escapedPrompt)
         val promptLength = prompt.length
         val maxTokenCount = 4096 - 1 - promptLength
 
