722c0f31e96a70c2da8dc444f7617664b3bfd96b
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-07-23 14:19:52 +0200

    :sparkles: Fix cgpt created test

--- docs/PITFALLS.md
+++ docs/PITFALLS.md
@@ -2,3 +2,4 @@
 
 1) When we send code to ChatGPT the indentation will break if the code we send contains this chars ['\n', '\t', '\r'].
    To fix this we need to replace them with a space ' '.
+2) https://intellij-support.jetbrains.com/hc/en-us/community/posts/360008653100-How-to-add-a-multi-button-in-table-s-toolbar-
--- src/test/kotlin/com/github/sloppylopez/moneypennyideaplugin/gineapigs/ClassThatNeedsRefactorTest.kt
+++ src/test/kotlin/com/github/sloppylopez/moneypennyideaplugin/gineapigs/ClassThatNeedsRefactorTest.kt
@@ -2,24 +2,16 @@ package com.github.sloppylopez.moneypennyideaplugin.gineapigs
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.*
 
 class ClassThatNeedsRefactorTest {
 
     @Test
     fun testGetPrimeNumbers() {
-        val classThatNeedsRefactor = ClassThatNeedsRefactor()
-
         val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
         val expectedPrimeNumbers = listOf(2, 3, 5, 7)
 
-        val classThatNeedsRefactorSpy = spy(classThatNeedsRefactor)
-        doReturn(true).`when`(classThatNeedsRefactorSpy).getPrimeNumbers(any())
-
-        val actualPrimeNumbers = classThatNeedsRefactorSpy.getPrimeNumbers(numbers)
+        val actualPrimeNumbers = ClassThatNeedsRefactor().getPrimeNumbers(numbers)
 
         assertEquals(expectedPrimeNumbers, actualPrimeNumbers)
-        verify(classThatNeedsRefactorSpy, times(10)).getPrimeNumbers(any())
     }
 }
