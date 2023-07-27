acc10d563f12789318c19ee464aa81f6c6993335
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-07-27 20:07:04 +0200

    :sparkles: Add proper spacing

--- src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/components/ChatWindowContent.kt
+++ src/main/kotlin/com/github/sloppylopez/moneypennyideaplugin/components/ChatWindowContent.kt
@@ -1,5 +1,6 @@
 package com.github.sloppylopez.moneypennyideaplugin.components
 
+import com.intellij.ui.JBColor
 import java.awt.BorderLayout
 import java.awt.Dimension
 import javax.swing.*
@@ -13,9 +14,14 @@ class ChatWindowContent : JPanel() {
         chatList = JList(listModel)
         chatList.cellRenderer = ChatCellRenderer()
         chatList.layoutOrientation = JList.VERTICAL
-//        chatList.fixedCellWidth = 670//This should be smaller than the width of the scroll pane to avoid horizontal scrolling
+        chatList.fixedCellWidth =
+            470//This should be smaller than the width of the scroll pane to avoid horizontal scrolling
         val scrollPane = JScrollPane(chatList)
         scrollPane.border = BorderFactory.createEmptyBorder(0, 0, 0, 0)
+        border = BorderFactory.createCompoundBorder(
+            BorderFactory.createLineBorder(JBColor.GRAY),
+            BorderFactory.createEmptyBorder(10, 10, 10, 10)
+        )
         add(scrollPane, BorderLayout.CENTER)
 //        preferredSize = null
     }
@@ -25,8 +31,8 @@ class ChatWindowContent : JPanel() {
     }
 
     override fun getPreferredSize(): Dimension {
-        val width = 700
-        val height = 200
+        val width = 500
+        val height = 350
         return Dimension(width, height)
     }
 }
