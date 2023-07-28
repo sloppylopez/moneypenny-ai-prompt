892c23e2072468dfe36099099361848cd3c42cd5
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-07-28 18:34:46 +0200

    :bug: remove deprecation

--- src/test/kotlin/com/github/sloppylopez/moneypennyideaplugin/gineapigs/NumbersCalculator.kt
+++ src/test/kotlin/com/github/sloppylopez/moneypennyideaplugin/gineapigs/NumbersCalculator.kt
@@ -1,7 +1,6 @@
 package com.github.sloppylopez.moneypennyideaplugin.gineapigs
 
 class NumbersCalculator {
-    fun getPrimeNumbers(numbers: Array<Int>): List<Int> = numbers.filter { number ->
-        number >= 2 && (2 until number).none { it -> number % it == 0 }
-    }
+
+    fun getPrimeNumbers(numbers: Array<Int>): List<Int> = numbers.filter { number -> number > 1 && (2 until number).none { number % it == 0 } }
 }
